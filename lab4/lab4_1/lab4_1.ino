7f02df8952258946155ed1be1f09b973679aa54a
sebvc <90735870+sebvc@users.noreply.github.com>
2022-09-22 17:40:40 -0500

    lab 4 file set up

--- lab4/lab4_1/lab4_1.ino
+++ lab4/lab4_1/lab4_1.ino
@@ -28,31 +28,25 @@
  * |----------------------|-----|---------|-------|--------------|---------|
  */
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-}
-// Declarations
-int num1=0, num2=0;
-
-void loop() {
-// put your main code here, to run repeatedly:
-//  Serial.println("Mechatronics");
-
-//Send data only when you receive data.
-//Wait until there is something to read:
-  while (Serial.available() == 0) {}
-  num1= Serial.read()-'0';
-  Serial.print("num1: ");
-  Serial.println(num1);
-  while (Serial.available() == 0) {}
-  num2= Serial.read()-'0';
-  Serial.print("num2: ");
-  Serial.println(num2);
-  
-// Say what you got:
-  Serial.print("sum= ");
-  Serial.println(num1+num2); 
-  
-  }
+#define led_pin 3 
+volatile unsigned long i; 
+ 
+void setup() { 
+  Serial.begin(9600); 
+  pinMode(led_pin, OUTPUT); 
+  // Create an interrupt service on interrupt 0 
+  attachInterrupt(0, isr, RISING); 
+} 
+ 
+void loop() { 
+  Serial.println("..."); 
+  delay(100); 
+} 
+ 
+void isr(){ 
+  Serial.println("External Interrupt"); 
+  digitalWrite(led_pin,HIGH); 
+  for(i=0;i<500000;i++); // Give some delay 
+  //delayMicroseconds(10000);  // another way to add delay 
+  digitalWrite(led_pin,LOW); 
+} 
--- lab4/lab4_2/lab4_2.ino
+++ lab4/lab4_2/lab4_2.ino
@@ -2,12 +2,27 @@
  * Assignment:
  * Tune the end count in the for loop using the oscilloscope so that the LED can be lit for 500 ms.  
  */
-  void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+// This is how we include non-standard libraries like Timer3 
+#include "TimerThree.h" 
+#define led_pin 3 
+volatile unsigned long i; 
+ 
+void setup() { 
+  Serial.begin(9600); 
+  pinMode(3,OUTPUT); 
+  Timer3.initialize(5000000); // time before overflow in us 
+  Timer3.attachInterrupt(isr); // isr triggered at overflow 
+} 
+ 
+void loop(){ 
+  Serial.println("..."); 
+  delay(100); 
+} 
+ 
+void isr(){ 
+  Serial.println("Timer 3 Interrupt"); 
+  digitalWrite(led_pin,HIGH); 
+  for(i=0;i<500000;i++); // Give some delay 
+  //delayMicroseconds(10000);  // another way to add delay 
+  digitalWrite(led_pin,LOW); 
+} 
