d691aeedcc44328cd0ba16ef55e74541ed631212
sebvc <90735870+sebvc@users.noreply.github.com>
2022-09-29 20:04:43 -0500

    Lab 5 completed. Co-authored-by: dracire <dracire@users.noreply.github.com> Co-authored-by: Alan-Gregory <Alan-Gregory@users.noreply.github.com>

--- lab5/lab5_1/lab5_1.ino
+++ lab5/lab5_1/lab5_1.ino
@@ -8,22 +8,172 @@
 
 #define pin_a 2
 #define pin_b 3
+char lastState, newState; // A or B
+bool stateA, stateB;
+unsigned int count = 0;
+bool dir = 0;        // direction of robot: 1:true:CW:A-Leading  & 0:false:CCW:B-leading
+bool stopped = true; // if robot moving last read alt to moving=false
+char arr[4];
+int index = 0;
+/* Order
+ * 1/CW : A -> D/neither -> B -> C/both |  
+ *      ^-------------------------|  
+ * B is leading
+ * 
+ * 0/CCW :A -> C/both -> B -> D?neither |  
+ *      ^-------------------------|  
+ * A is leading 
+ * 
+ */
+
+
+
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(pin_a, INPUT);
     pinMode(pin_b, INPUT);
+
+    lastState = Update(stateA,stateB);
+
 }
 
 void loop()
 {
-    if (digitalRead(pin_a))
+    delay(10);
+    stateA = digitalRead(pin_a);
+    stateB = digitalRead(pin_b);
+    newState = Update(stateA,stateB);
+    
+    switch (lastState)
     {
-        Serial.println("A");
+    case 'A':
+        if (newState=='A')
+            stopped=true;
+        else if (newState=='D') // neither
+        {
+            stopped=false;
+            dir = 1; // CW
+            count++;
+        }
+        else if (newState=='C') // both
+        {
+            stopped=false;
+            dir = 0; // CCW
+            count--;
+        }
+        break;
+    
+    case 'B':
+        if (newState=='B')
+            stopped=true;
+        else if (newState=='C') // both
+        {
+            stopped=false;
+            dir = 1; // CW
+            count++;
+        }
+        else if (newState=='D') // neither
+        {
+            stopped=false;
+            dir = 0; // CCW
+            count--;
+
+        }
+        break;
+    
+    case 'C': // both
+        if (newState=='C')// both
+            stopped=true;
+        else if (newState=='A') 
+        {
+            stopped=false;
+            dir = 1; // CW
+            count++;
+        }
+        else if (newState=='B') 
+        {
+            stopped=false;
+            dir = 0; // CCW
+            count--;
+        }
+        break;
+    
+    case 'D': // neither
+        if (newState=='D')// neither
+            stopped=true;
+        else if (newState=='B') 
+        {
+            stopped=false;
+            dir = 1; // CW
+            count++;
+        }
+        else if (newState=='A') 
+        {
+            stopped=false;
+            dir = 0; // CCW
+            count--;
+        }
+        break;
+    
+    default:
+        break;
     }
-    if (digitalRead(pin_b))
+    //mod count to encoder revolution scale
+    count %= 64;
+
+    Serial.print("count: ");
+    Serial.print(count);
+    Serial.print('\t');
+    Serial.print( dir? "CW\t" : "CCW\t");
+    Serial.println(stopped? "Stopped" : "Moving");
+
+    lastState = newState;
+
+    //         if (dir == 0){
+
+    //         count += 1
+    //     }
+    //     Serial.println("A");
+    //     dir = 1
+    // }
+    // else if (digitalRead(pin_b))
+    // {
+    //      if (dir == 1){
+
+    //         count += 1
+    //     }
+    //     Serial.println("B");
+    //     dir = 0
+    // }
+    // delay(5);
+}
+
+
+char Update(bool stateA, bool stateB)
+{
+    char last;
+    if (stateA && stateB)
     {
-        Serial.println("B");
+        last='C';//both
     }
+    else if ((!stateA) && (!stateB))
+    {
+        last = 'D';//neither
+    }
+    else
+    {
+
+        if (stateA)
+        {
+            last = 'A';
+        }
+        if (stateB)
+        {
+            last ='B';
+        }
+    }
+    //Serial.println(last);
+    return last;
 }
--- lab5/lab5_2/lab5_2.ino
+++ lab5/lab5_2/lab5_2.ino
@@ -8,6 +8,11 @@
 
 #define pin_a 2
 #define pin_b 3
+int count = 0;
+bool dir = 0;
+int state = 0;
+int prevstate = 0;
+
 void setup()
 {
     pinMode(pin_a, INPUT);
@@ -22,9 +27,59 @@ void setup()
 
 void loop()
 {
+    switch (dir)
+    {
+    case 1:
+        Serial.print("Clockwise   ");
+        break;
+    case 0:
+        Serial.print("Counter Clockwise   ");
+        break;
+    }
+        Serial.println(count);
     // display the encoder count and direction
 }
 void doEncoder()
 {
+    bool stateA = digitalRead(pin_a);
+    bool stateB = digitalRead(pin_b);
+    if (stateA > stateB){
+        state = 0;
+    }
+    if (stateA < stateB){
+        state = 1;
+    }
+    if (!stateA && !stateB){
+        state = 2;
+    }
+    if (stateA && stateB){
+        state = 3;
+    }
+    
+    switch (state)
+    {
+    case 0:
+        if (prevstate == 2){
+            dir = 0;
+            count--;
+        }
+        if (prevstate == 3){
+            dir = 1;
+            count++;
+        }    
+        break;
+    case 1:
+        if (prevstate == 2){
+            dir = 1;
+            count++;
+        }
+        if (prevstate == 3){
+            dir = 0;
+            count--;
+        }    
+        break;
+
+    }
+    prevstate = state;
     // your ISR to count and determine direction
 }
